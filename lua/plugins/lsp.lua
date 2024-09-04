7f2ea2791020698b4785c580505b2a6b65dfb71d
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2024-09-04 23:44:14 +0900

    remove unused config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -135,32 +135,6 @@ return {
         root_dir = util.root_pattern('.git', '.marksman.toml', '_quarto.yml'),
       }
 
-      lspconfig.r_language_server.setup {
-        capabilities = capabilities,
-        flags = lsp_flags,
-        settings = {
-          r = {
-            lsp = {
-              rich_documentation = false,
-            },
-          },
-        },
-      }
-
-      lspconfig.cssls.setup {
-        capabilities = capabilities,
-        flags = lsp_flags,
-      }
-
-      lspconfig.html.setup {
-        capabilities = capabilities,
-        flags = lsp_flags,
-      }
-
-      lspconfig.emmet_language_server.setup {
-        capabilities = capabilities,
-        flags = lsp_flags,
-      }
 
       lspconfig.yamlls.setup {
         capabilities = capabilities,
@@ -179,43 +153,7 @@ return {
         capabilities = capabilities,
         flags = lsp_flags,
       }
-
-      lspconfig.dotls.setup {
-        capabilities = capabilities,
-        flags = lsp_flags,
-      }
-
-      lspconfig.tsserver.setup {
-        capabilities = capabilities,
-        flags = lsp_flags,
-        filetypes = { 'js', 'javascript', 'typescript', 'ojs' },
-      }
-
-      local function get_quarto_resource_path()
-        local function strsplit(s, delimiter)
-          local result = {}
-          for match in (s .. delimiter):gmatch('(.-)' .. delimiter) do
-            table.insert(result, match)
-          end
-          return result
-        end
-
-        local f = assert(io.popen('quarto --paths', 'r'))
-        local s = assert(f:read '*a')
-        f:close()
-        return strsplit(s, '\n')[2]
-      end
-
-      local lua_library_files = vim.api.nvim_get_runtime_file('', true)
-      local lua_plugin_paths = {}
-      local resource_path = get_quarto_resource_path()
-      if resource_path == nil then
-        vim.notify_once 'quarto not found, lua library files not loaded'
-      else
-        table.insert(lua_library_files, resource_path .. '/lua-types')
-        table.insert(lua_plugin_paths, resource_path .. '/lua-plugin/plugin.lua')
-      end
-
+ 
       lspconfig.lua_ls.setup {
         capabilities = capabilities,
         flags = lsp_flags,
@@ -245,11 +183,7 @@ return {
         },
       }
 
-      lspconfig.julials.setup {
-        capabilities = capabilities,
-        flags = lsp_flags,
-      }
-
+      
       lspconfig.bashls.setup {
         capabilities = capabilities,
         flags = lsp_flags,
