4550007ce623426bae64f3414f16b9a154a8b089
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2025-06-08 16:30:54 +0900

    update fom python

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -28,15 +28,73 @@ return {
         'rcarriga/nvim-dap-ui',
         'mfussenegger/nvim-dap-python',
         'theHamsta/nvim-dap-virtual-text',
+        -- nvim-dap-ui is too big for smaller termial
+        -- {
+        --   'igorlfs/nvim-dap-view',
+        --   opts = {
+        --     winbar = {
+        --       show = true,
+        --       sections = { 'watches', 'exceptions', 'breakpoints', 'threads', 'repl' },
+        --       -- Must be one of the sections declared above
+        --       default_section = 'watches',
+        --     },
+        --     windows = {
+        --       height = 10,
+        --       terminal = {
+        --         -- 'left'|'right'|'above'|'below': Terminal position in layout
+        --         position = 'left',
+        --         -- List of debug adapters for which the terminal should be ALWAYS hidden
+        --         hide = {},
+        --         -- Hide the terminal when starting a new session
+        --         start_hidden = false,
+        --       },
+        --     },
+        --   },
+        -- },
       },
     },
     config = function()
       vim.fn.sign_define('DapBreakpoint', { text = '🦆', texthl = '', linehl = '', numhl = '' })
       local dap = require 'dap'
       local ui = require 'dapui'
-      require('dapui').setup()
+      -- https://github.com/rcarriga/nvim-dap-ui/issues/320
+      local ui_config = {
+        icons = { expanded = '📖', collapsed = '📕', current_frame = '👉' },
+        controls = {
+          icons = {
+            pause = '⏸️',
+            play = '⏯️',
+            step_into = '↴',
+            step_over = '↷',
+            step_out = '↑',
+            step_back = '↶',
+            run_last = '🔁',
+            terminate = '❌',
+            disconnect = '🆘',
+          },
+        },
+        layouts = {
+          {
+            elements = {
+              {
+                id = 'repl',
+                size = 0.5,
+              },
+              {
+                id = 'console',
+                size = 0.5,
+              },
+            },
+            position = 'bottom',
+            size = 10,
+          },
+        },
+      }
+
+      require('dapui').setup(ui_config)
+      -- require('dapui').setup()
       require('dap-python').setup()
-      require('dap.ext.vscode').load_launchjs 'launch.json'
+      require('dap.ext.vscode').load_launchjs()
 
       require('nvim-dap-virtual-text').setup {
         -- Hides tokens, secrets, and other sensitive information
@@ -57,18 +115,21 @@ return {
         end,
       }
 
-      dap.listeners.before.attach.dapui_config = function()
-        ui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        ui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        ui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        ui.close()
-      end
+      -- require('nvim-dap-view').setup {
+      --
+      -- }
+      -- dap.listeners.before.attach.dapui_config = function()
+      --   ui.open()
+      -- end
+      -- dap.listeners.before.launch.dapui_config = function()
+      --   ui.open()
+      -- end
+      -- dap.listeners.before.event_terminated.dapui_config = function()
+      --   ui.close()
+      -- end
+      -- dap.listeners.before.event_exited.dapui_config = function()
+      --   ui.close()
+      -- end
     end,
     keys = {
       { '<leader>db', ":lua require'dap'.toggle_breakpoint()<cr>", desc = 'debug [b]reakpoint' },
@@ -76,8 +137,18 @@ return {
       { '<leader>do', ":lua require'dap'.step_over()<cr>", desc = 'debug [o]ver' },
       { '<leader>dO', ":lua require'dap'.step_out()<cr>", desc = 'debug [O]ut' },
       { '<leader>di', ":lua require'dap'.step_into()<cr>", desc = 'debug [i]nto' },
+      { '<F5>', ":lua require'dap'.step_into()<cr>", desc = 'debug into' },
+      { '<F6>', ":lua require'dap'.step_over()<cr>", desc = 'debug over' },
+      { '<F7>', ":lua require'dap'.step_out()<cr>", desc = 'debug out' },
+      { '<F8>', ":lua require'dap'.continue()<cr>", desc = 'debug continue' },
+      -- { '<F9>', ":lua require'dap-view'.toggle()<cr>", desc = 'toogle dap-view ui' },
+      { '<F9>', ":lua require'dapui'.toggle()<cr>", desc = 'toogle dap-view ui' },
+      { '<F10>', ":lua require'dap'.toggle_breakpoint()<cr>", desc = 'debug breakpoint' },
       { '<leader>dr', ":lua require'dap'.repl_open()<cr>", desc = 'debug [r]epl' },
       { '<leader>du', ":lua require'dapui'.toggle()<cr>", desc = 'debug [u]i' },
+      -- { 'ge', ":lua require'dap.ui.widgets'.hover()<cr>", desc = 'debug hov[e]r' },
+      { 'ge', ":lua require'dapui'.eval()<cr>", desc = 'debug [e]val' },
+      { 'gp', ":lua require'dapui'.eval(require'dapui.util'.get_current_expr() .. '.shape')<cr>", 'debug eval shape' },
     },
   },
 }
