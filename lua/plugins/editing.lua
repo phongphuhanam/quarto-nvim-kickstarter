5be25ff89c4814402017a579f4620838be114c08
PhuNinh <18023194+phongphuhanam@users.noreply.github.com>
2025-07-09 09:48:09 +0900

    use ruff formatter

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -38,7 +38,14 @@ return {
         -- },
         formatters_by_ft = {
           lua = { 'stylua' },
-          python = { 'isort', 'black' },
+          python = function(bufnr)
+            if require('conform').get_formatter_info('ruff_format', bufnr).available then
+              return { 'ruff_format' }
+            else
+              return { 'isort', 'black' }
+            end
+          end,
+          -- python = { 'isort', 'black' },
           json = { 'fixjson' },
           quarto = { 'injected' },
           markdown = { 'injected' },
