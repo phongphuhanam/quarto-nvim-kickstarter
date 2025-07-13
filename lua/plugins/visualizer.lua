eb5c01b4f58eb773354882d7d3e68d2bac23a537
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2025-07-13 15:11:03 +0900

    add tiny-inline-diagnostic

--- lua/plugins/visualizer.lua
+++ lua/plugins/visualizer.lua
@@ -21,4 +21,13 @@ return {
     },
     cmd = { 'CsvViewEnable', 'CsvViewDisable', 'CsvViewToggle' },
   },
+  {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy', -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      require('tiny-inline-diagnostic').setup()
+      vim.diagnostic.config { virtual_text = false } -- Only if needed in your configuration, if you already have native LSP diagnostics
+    end,
+  },
 }
