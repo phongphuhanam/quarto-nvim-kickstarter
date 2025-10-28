48996d67b7532cd581b5e8335f3f215336ad107c
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2025-10-28 12:08:58 +0900

    re-write keymap for some python operations

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -313,6 +313,34 @@ local function get_otter_symbols_lang()
   vim.lsp.buf_request(main_nr, ms.textDocument_documentSymbol, params, nil)
 end
 
+-- opencv python related dap
+local function eval_current_expr(fn, desc)
+  local dapui = require 'dapui'
+  local util = require 'dapui.util'
+
+  local expr = util.get_current_expr()
+  if not expr or expr == '' then
+    vim.notify('No current expression', vim.log.levels.WARN)
+    return
+  end
+  -- Build the Lua code to run
+  local code = fn(expr)
+  dapui.eval(code)
+end
+
+vim.keymap.set('n', 'gp', function()
+  eval_current_expr(function(expr)
+    return expr .. '.shape'
+  end, 'debug eval shape')
+end, { desc = 'debug eval shape' })
+
+vim.keymap.set('n', 'g1', function()
+  eval_current_expr(function(expr)
+    local path = '/dev/shm/' .. expr .. '.png'
+    return string.format('cv2.imwrite("%s", %s)', path, expr)
+  end, 'save image with opencv')
+end, { desc = 'save image with opencv' })
+
 vim.keymap.set('n', '<leader>os', get_otter_symbols_lang, { desc = 'otter [s]ymbols' })
 
 local function toggle_conceal()
