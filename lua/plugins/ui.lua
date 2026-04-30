5c5da7309c973d34cffb0db7451d1476c24c4dc5
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2026-04-30 16:47:49 +0900

    change to barrettruth/canola.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -171,9 +171,9 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = false },
   },
-
+  -- https://github.com/barrettruth/canola.nvim
   { -- edit the file system as a buffer
-    'stevearc/oil.nvim',
+    'barrettruth/canola.nvim',
     opts = {
       keymaps = {
         ['<C-s>'] = false,
