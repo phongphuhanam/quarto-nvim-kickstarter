0b62fc56d15a805a63e97f07e4a87ed5b3ad61de
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2026-05-16 09:41:53 +0900

    updaet

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -6,6 +6,7 @@ return {
     init = function()
       -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_view_forward_search_on_start = 0
 
       vim.g.vimtex_compiler_method = 'latexmk'
       vim.g.vimtex_compiler_latexmk = {
@@ -15,10 +16,19 @@ return {
 
       vim.g.vimtex_doc_enabled = 0
       vim.g.vimtex_complete_enabled = 0
-      vim.g.vimtex_syntax_enabled = 0
       vim.g.vimtex_imaps_enabled = 0
 
-      vim.g.vimtex_view_forward_search_on_start = 0
+      vim.g.vimtex_syntax_enabled = 1
+      vim.g.vimtex_syntax_conceal_disable = 1
+      -- Configure concealment (replaces LaTeX commands with symbols like α)
+      -- vim.g.vimtex_syntax_conceal = {
+      --   additions = 1,
+      --   bibtex = 1,
+      --   greek = 1,
+      --   math_bounds = 1,
+      --   sections = 1,
+      --   styles = 1,
+      -- }
       -- vim.g.vimtex_view_method = 'general'
       -- vim.g.vimtex_view_general_options = 'okular'
       -- vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
