7fa9d83faa4a0c6a35a0c5739f5952824fedfc60
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2026-06-03 16:40:13 +0900

    update config

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -14,6 +14,12 @@ return {
       -- In init.lua
       vim.g.tex_flavor = 'latex'
       vim.g.vimtex_view_method = 'zathura'
+
+      -- vim.g.vimtex_quickfix_open_on_warning = 0
+      vim.g.vimtex_quickfix_mode = 0
+      -- let g:vimtex_view_method = 'zathura'
+      -- vim.g.vimtex_view_zathura_options = '--mode fullscreen'
+
       vim.g.vimtex_view_forward_search_on_start = 0
 
       vim.g.vimtex_compiler_method = 'latexmk'
